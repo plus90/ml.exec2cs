eacf925db3c53368106b706d32e829cb7f0fa2f1
dariushha <b.g.dariush+repo@gmail.com>
2014-12-03 04:43:53 +0330

    Init VisualStudio

---
+++ mltak2/grid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace mltak2
+{
+    public partial class grid : Form
+    {
+        public grid()
+        {
+            InitializeComponent();
+        }
+    }
+}
