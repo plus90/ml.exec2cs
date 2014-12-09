b90757139868d6412e2ae241990a8c207339114b
dariushha <b.g.dariush+repo@gmail.com>
2014-12-09 17:44:34 +0330

    @ ReinforcementLearning/ADP.cs: doced

--- ReinforcementLearning/ADP.cs
+++ ReinforcementLearning/ADP.cs
@@ -1,20 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Environment;
 using System.Collections;
+using System.Collections.Generic;
+using Environment;
 
 namespace ReinforcementLearning
 {
-    using QVal = System.Single;
-    using State = System.Drawing.Point;
-    using Reward = Int16;
     using Action = Environment.GridHelper.Directions;
     using NVal = System.Single;
-    using UVal = System.Single;
+    using Reward = Int16;
+    using State = System.Drawing.Point;
     using StateAction = KeyValuePair<System.Drawing.Point, Environment.GridHelper.Directions>;
     using StateActionState = KeyValuePair<System.Drawing.Point, KeyValuePair<Environment.GridHelper.Directions, System.Drawing.Point>>;
+    using UVal = System.Single;
+    /// <summary>
+    /// The ADP algorithm
+    /// <see cref="docs/ADP.pdf"/>
+    /// </summary>
     public class ADP : RLearning, IUtility
     {
         /// <summary>
@@ -38,7 +39,7 @@ namespace ReinforcementLearning
         /// </summary>
         public State InitialState { get; set; }
         /// <summary>
-        /// Construct a Q-learner instance
+        /// Construct a ADP instance
         /// </summary>
         /// <param name="grid">The grid instance which trying to learn</param>
         /// <param name="A">The list of valid actions</param>
@@ -47,7 +48,7 @@ namespace ReinforcementLearning
         /// <param name="QTable">The initial Q-table(Can be also `null`)</param>
         public ADP(Grid grid, List<Action> A, float gamma, float alpha, Hashtable QSA) : this(grid, A, gamma, alpha) { if (QSA != null)this.QTable = QSA; }
         /// <summary>
-        /// Construct a Q-learner instance
+        /// Construct a ADP instance
         /// </summary>
         /// <param name="grid">The grid instance which trying to learn</param>
         /// <param name="A">The list of valid actions</param>
@@ -64,132 +65,80 @@ namespace ReinforcementLearning
 
         }
         /// <summary>
-        /// Construct a Q-learner instance
+        /// Construct a ADP instance
         /// </summary>
-        /// <param name="grid">The grid instance which trying to learn</param>
-        /// <param name="A">The list of valid actions</param>
-        /// <param name="gamma">The discount factor</param>
-        /// <param name="alpha">The learning rate</param>
-        /// <param name="lambda">The lambda rate</param>
-        /// <param name="QSA">The initial Q-Table</param>
-        /// <param name="QTable">The initial Q-table(Can be also `null`)</param>
+        /// <param name="r">The Reinforcement learner instance</param>
         public ADP(RLearning r) : this(r.Grid, r.Actions, r.Gamma, r.Alpha, r.QTable) { this.VisitedStateActions = r.VisitedStateActions; }
         /// <summary>
-        /// Sets eligibility trace value
+        /// Sets U-Table's value
         /// </summary>
         /// <param name="s">The state</param>
-        /// <param name="a">The action</param>
-        /// <param name="e">The eligibility value to be updated</param>
-        protected virtual void __set_u_value(State s, UVal e)
-        {
-            var sig = s;
-            if (!this.UTable.Contains(sig))
-                this.UTable.Add(sig, e);
-            else this.UTable[sig] = e;
-        }
+        /// <param name="e">The value to be updated</param>
+        protected virtual void __set_u_value(State s, UVal e) { var sig = s; if (!this.UTable.Contains(sig)) this.UTable.Add(sig, e); else this.UTable[sig] = e; }
         /// <summary>
-        /// Gets UTable if any exists or initializes it by 0.
+        /// Gets U-Table if any exists or initializes it by 0.
         /// </summary>
         /// <param name="s">The state</param>
-        /// <param name="a">The action</param>
-        /// <returns>The eligibility trace value</returns>
-        protected virtual UVal __get_u_value(State s)
-        {
-            var sig = s;
-            if (!this.UTable.Contains(sig))
-                this.UTable.Add(sig, (UVal)0);
-            return (UVal)this.UTable[sig];
-        }
-        ///// <summary>
-        /// Sets eligibility trace value
-        /// </summary>
-        /// <param name="s">The state</param>
-        /// <param name="a">The action</param>
-        /// <param name="e">The eligibility value to be updated</param>
-        protected virtual void __set_nsa_value(State s, Action a, NVal e)
-        {
-            var sig = new StateAction(s, a);
-            if (!this.NSA.Contains(sig))
-                this.NSA.Add(sig, e);
-            else this.NSA[sig] = e;
-        }
+        /// <returns>The U-table's value</returns>
+        protected virtual UVal __get_u_value(State s) { var sig = s; if (!this.UTable.Contains(sig)) this.UTable.Add(sig, (UVal)0); return (UVal)this.UTable[sig]; }
         /// <summary>
-        /// Gets UTable if any exists or initializes it by 0.
+        /// Sets N[s,a]'s value
         /// </summary>
         /// <param name="s">The state</param>
         /// <param name="a">The action</param>
-        /// <returns>The eligibility trace value</returns>
-        protected virtual NVal __get_nsa_value(State s, Action a)
-        {
-            var sig = new StateAction(s, a);
-            if (!this.NSA.Contains(sig))
-                this.NSA.Add(sig, (NVal)0);
-            return (NVal)this.NSA[sig];
-        }
-        ///// <summary>
-        /// Sets eligibility trace value
-        /// </summary>
-        /// <param name="s">The state</param>
-        /// <param name="a">The action</param>
-        /// <param name="e">The eligibility value to be updated</param>
-        protected virtual void __set_nsas_value(State s, Action a, State sprim, NVal e)
-        {
-            var sig = new StateActionState(s, new KeyValuePair<Action, State>(a, sprim));
-            if (!this.NSAS.Contains(sig))
-                this.NSAS.Add(sig, e);
-            else this.NSAS[sig] = e;
-        }
+        /// <param name="e">The value to be updated</param>
+        protected virtual void __set_nsa_value(State s, Action a, NVal e) { var sig = new StateAction(s, a); if (!this.NSA.Contains(sig)) this.NSA.Add(sig, e); else this.NSA[sig] = e; }
         /// <summary>
-        /// Gets UTable if any exists or initializes it by 0.
+        /// Gets N[s,a]'s value if any exists or initializes it by 0.
         /// </summary>
         /// <param name="s">The state</param>
         /// <param name="a">The action</param>
-        /// <returns>The eligibility trace value</returns>
-        protected virtual NVal __get_t_value(State s, Action a, State sprim)
-        {
-            var sig = new StateActionState(s, new KeyValuePair<Action, State>(a, sprim));
-            if (!this.TTable.Contains(sig))
-                this.TTable.Add(sig, (NVal)0);
-            return (NVal)this.TTable[sig];
-        }
+        /// <returns>The value</returns>
+        protected virtual NVal __get_nsa_value(State s, Action a) { var sig = new StateAction(s, a); if (!this.NSA.Contains(sig)) this.NSA.Add(sig, (NVal)0); return (NVal)this.NSA[sig]; }
         /// <summary>
-        /// Sets eligibility trace value
+        /// Sets N[s,a, s']'s trace value
         /// </summary>
         /// <param name="s">The state</param>
         /// <param name="a">The action</param>
-        /// <param name="e">The eligibility value to be updated</param>
-        protected virtual void __set_t_value(State s, Action a, State sprim, NVal e)
-        {
-            var sig = new StateActionState(s, new KeyValuePair<Action, State>(a, sprim));
-            if (!this.TTable.Contains(sig))
-                this.TTable.Add(sig, e);
-            else this.TTable[sig] = e;
-        }
+        /// <param name="sprim">The next state</param>
+        /// <param name="e">The value to be updated</param>
+        protected virtual void __set_nsas_value(State s, Action a, State sprim, NVal e) { var sig = new StateActionState(s, new KeyValuePair<Action, State>(a, sprim)); if (!this.NSAS.Contains(sig)) this.NSAS.Add(sig, e); else this.NSAS[sig] = e; }
         /// <summary>
-        /// Gets UTable if any exists or initializes it by 0.
+        /// Gets T-Table if any exists or initializes it by 0.
         /// </summary>
         /// <param name="s">The state</param>
         /// <param name="a">The action</param>
-        /// <returns>The eligibility trace value</returns>
-        protected virtual NVal __get_nsas_value(State s, Action a, State sprim)
-        {
-            var sig = new StateActionState(s, new KeyValuePair<Action, State>(a, sprim));
-            if (!this.NSAS.Contains(sig))
-                this.NSAS.Add(sig, (NVal)0);
-            return (NVal)this.NSAS[sig];
-        }
-        protected void __visit(State s, Action a, State sprim)
-        {
-            var sig = new StateAction(s, a);
-            if (!this.VisitedStateActions.Contains(sig))
-                this.VisitedStateActions.Add(sig, new List<State>() { sprim });
-            else (this.VisitedStateActions[sig] as List<State>).Add(sprim);
-        }
+        /// <param name="sprim">The next state</param>
+        /// <returns>The T-value</returns>
+        protected virtual NVal __get_t_value(State s, Action a, State sprim) { var sig = new StateActionState(s, new KeyValuePair<Action, State>(a, sprim)); if (!this.TTable.Contains(sig)) this.TTable.Add(sig, (NVal)0); return (NVal)this.TTable[sig]; }
+        /// <summary>
+        /// Sets T-Value's value
+        /// </summary>
+        /// <param name="s">The state</param>
+        /// <param name="a">The action</param>
+        /// <param name="sprim">The next state</param>
+        /// <param name="e">The value to be updated</param>
+        protected virtual void __set_t_value(State s, Action a, State sprim, NVal e) { var sig = new StateActionState(s, new KeyValuePair<Action, State>(a, sprim)); if (!this.TTable.Contains(sig)) this.TTable.Add(sig, e); else this.TTable[sig] = e; }
+        /// <summary>
+        /// Gets N[s,a,s']'s if any exists or initializes it by 0.
+        /// </summary>
+        /// <param name="s">The state</param>
+        /// <param name="a">The action</param>
+        /// <param name="sprim">The next state</param>
+        /// <returns>The value</returns>
+        protected virtual NVal __get_nsas_value(State s, Action a, State sprim) { var sig = new StateActionState(s, new KeyValuePair<Action, State>(a, sprim)); if (!this.NSAS.Contains(sig)) this.NSAS.Add(sig, (NVal)0); return (NVal)this.NSAS[sig]; }
+        /// <summary>
+        /// Mark a [s,a] visited and the result will be [s']
+        /// </summary>
+        /// <param name="s">The state</param>
+        /// <param name="a">The action</param>
+        /// <param name="sprim">The next state</param>
+        protected void __visit(State s, Action a, State sprim) { var sig = new StateAction(s, a); if (!this.VisitedStateActions.Contains(sig)) this.VisitedStateActions.Add(sig, new List<State>() { sprim }); else (this.VisitedStateActions[sig] as List<State>).Add(sprim); }
         /// <summary>
         /// Learn the grid
         /// </summary>
         /// <param name="termination_validtor">The learning terminator validator; if it returns true the learning operation will halt.</param>
-        /// <returns>The learned policy</returns>
+        /// <returns>The learned utility</returns>
         public override Hashtable Learn(Func<Grid, State, long, bool> termination_validtor = null)
         {
             // start the refresher's timer
@@ -202,6 +151,7 @@ namespace ReinforcementLearning
             State state = this.InitialState;
             // define an initial action
             Action a = Action.HOLD;
+            // re-init the visited state-action table
             this.VisitedStateActions = new Hashtable();
             do
             {
@@ -211,11 +161,13 @@ namespace ReinforcementLearning
                 var s = this.GridHelper.Move(state, a);
                 // get the new-state's reward
                 var r = this.__get_reward(s.NewPoint);
-                // update the Q-Value of current with [s, r, a, s'] values
+                // update the T-Value of current with [s, r, a, s'] values
                 this.__update_t_value(s.OldPoint, r, a, s.NewPoint);
+                // update the U-Value of current with [s, r, a, s'] values
                 this.__update_u_value(s.OldPoint, r, a, s.NewPoint); 
                 // assign the next state
                 state = s.NewPoint;
+                // mark visited state
                 this.__visit(s.OldPoint, a, s.NewPoint);
                 // examine the learning loop
             } while (!termination_validtor(this.Grid, state, this.StepCounter) && ++this.StepCounter <= long.MaxValue);
@@ -231,14 +183,11 @@ namespace ReinforcementLearning
         /// <param name="r">The awarded reward at `t+1`</param>
         /// <param name="a">The taken action at `t`</param>
         /// <param name="stplus">The state at `t+1`</param>
-        /// <returns>The updated V-Value</returns>
+        /// <returns>The updated U-Value</returns>
         private void __update_u_value(State st, Reward r, Action a, State stplus)
         {
             UVal k = 0.0F;
-            foreach (var action in this.Actions)
-            {
-                k += this.__get_t_value(st, action, stplus) * this.__get_u_value(stplus);
-            }
+            foreach (var action in this.Actions) k += this.__get_t_value(st, action, stplus) * this.__get_u_value(stplus);
             this.__set_u_value(st, r + this.Gamma * k);
         }
         /// <summary>
@@ -248,13 +197,11 @@ namespace ReinforcementLearning
         /// <param name="r">The awarded reward at `t+1`</param>
         /// <param name="a">The taken action at `t`</param>
         /// <param name="stplus">The state at `t+1`</param>
-        /// <returns>The updated V-Value</returns>
+        /// <returns>The updated T-Value</returns>
         protected void __update_t_value(State st, Reward r, Action a, State stplus)
         {
             if (!this.UTable.Contains(st))
-            {
                 this.__set_u_value(st, r);
-            }
             else
             {
                 this.__set_nsa_value(st, a, this.__get_nsa_value(st, a) + 1);
