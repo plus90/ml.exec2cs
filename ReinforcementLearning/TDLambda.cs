ddb50e43c3aafba5ad4902db45ebab84f2db1f2e
dariushha <b.g.dariush+repo@gmail.com>
2014-12-09 18:19:44 +0330

    @ ReinforcementLearning/TDLambda.cs: doced and hot-fix

--- ReinforcementLearning/TDLambda.cs
+++ ReinforcementLearning/TDLambda.cs
@@ -1,16 +1,14 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Collections;
 using Environment;
 
 namespace ReinforcementLearning
 {
-    using QVal = System.Single;
-    using State = System.Drawing.Point;
-    using Reward = Int16;
     using Action = Environment.GridHelper.Directions;
+    using Reward = Int16;
+    using State = System.Drawing.Point;
     using VVal = System.Single;
     public class TDLambda : RLambdaLearning, IUtility
     {
@@ -46,7 +44,7 @@ namespace ReinforcementLearning
         /// </summary>
         /// <param name="r">The Reinforcement learner instance</param>
         /// <param name="lambda">The lambda factor</param>
-        public TDLambda(RLearning r, float lambda) : this(r.Grid, r.Actions, r.Gamma, r.Alpha, lambda, r.QTable) { this.VisitedStateActions = r.VisitedStateActions; }
+        public TDLambda(RLearning r, float lambda) : this(r.Grid, r.Actions, r.Gamma, r.Alpha, lambda, r.QTable) { this.VisitedStateActions = r.VisitedStateActions; this.VisitedState = r.VisitedState; }
         /// <summary>
         /// Sets eligibility trace value
         /// </summary>
@@ -101,7 +99,7 @@ namespace ReinforcementLearning
                 // get the new-state's reward
                 var r = this.__get_reward(s.NewPoint);
                 // update the Q-Value of current with [s, r, s'] values
-                this.__update_v_value(s.OldPoint, r, s.NewPoint);
+                this.__update_u_value(s.OldPoint, r, s.NewPoint);
                 // assign the next state
                 state = s.NewPoint;
                 // examine the learning loop
@@ -112,13 +110,13 @@ namespace ReinforcementLearning
             return this.UTable;
         }
         /// <summary>
-        /// Updates the V-Value
+        /// Updates the U-Value
         /// </summary>
         /// <param name="st">The state at `t`</param>
         /// <param name="r">The awarded reward at `t+1`</param>
         /// <param name="stplus">The state at `t+1`</param>
-        /// <returns>The updated V-Value</returns>
-        protected VVal __update_v_value(State st, Reward r, State stplus)
+        /// <returns>The updated U-Value</returns>
+        protected VVal __update_u_value(State st, Reward r, State stplus)
         {
             // δ ← r + γ * V(s') - V(s)
             var delta = r + this.Gamma * this.__get_u_value(stplus) - this.__get_u_value(st);
@@ -130,7 +128,7 @@ namespace ReinforcementLearning
             {
                 var s = keys[i];
                 // V(s) ← V(s, a) + αδe(s)
-                this.__set_u_value(s, (VVal)this.UTable[s] + this.Alpha * delta * this.__get_elig_value(s));
+                this.__set_u_value(s, this.__get_u_value(s) + this.Alpha * delta * this.__get_elig_value(s));
                 // e(s, a) ← γλe(s, a)
                 this.__set_elig_value(s, this.Gamma * this.Lambda * this.__get_elig_value(s));
             }
